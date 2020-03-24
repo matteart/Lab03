28358b66f41109951326a408f4f900dad8530893
matte <matte@DESKTOP-K6SEPUI>
2020-03-24 13:53:45 +0100

    Esercizio prima parte

--- Lab03_SpellChecker/src/main/java/it/polito/tdp/spellchecker/FXMLController.java
+++ Lab03_SpellChecker/src/main/java/it/polito/tdp/spellchecker/FXMLController.java
@@ -1,25 +1,110 @@
-/**
- * Sample Skeleton for 'Scene.fxml' Controller Class
- */
-
 package it.polito.tdp.spellchecker;
 
 import java.net.URL;
+import java.util.*;
 import java.util.ResourceBundle;
+
+import it.polito.tdp.spellchecker.model.Model;
+import it.polito.tdp.spellchecker.model.RichWord;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextArea;
+import javafx.scene.text.Text;
 
 public class FXMLController {
-
-    @FXML // ResourceBundle that was given to the FXMLLoader
+	ObservableList<String> list = FXCollections.observableArrayList("English", "Italiano");
+    private Model model;
+    String resultTXT="";
+    
+    @FXML
     private ResourceBundle resources;
 
-    @FXML // URL location of the FXML file that was given to the FXMLLoader
+    @FXML
     private URL location;
 
-    @FXML // This method is called by the FXMLLoader when initialization is complete
-    void initialize() {
+    @FXML
+    private ChoiceBox<String> choicheBox;
 
+    @FXML
+    private TextArea txtInsert;
+
+    @FXML
+    private Button btnSpell;
+
+    @FXML
+    private TextArea txtResult;
+
+    @FXML
+    private Text txtError;
+
+    @FXML
+    private Button btnClear;
+
+    @FXML
+    private Text txtTime;
+
+    @FXML
+    void handleClear(ActionEvent event) {
+    	Long start = System.nanoTime();
+    	
+      resultTXT="";
+      txtResult.setText(resultTXT);
+      
+      txtError.setText("Ci sono "+0+" errori");
+     
+      Long end = System.nanoTime();     
+      txtTime.setText("Tempo impiegato :" + this.checkTime(start, end)+ "ms");
+    }
+
+    @FXML
+    void handleSpell(ActionEvent event) {
+    	Long start = System.nanoTime();
+    	List <String> inputTextList = new LinkedList<String>();   	
+    	
+    	
+        String lingua = choicheBox.getValue();
+        model.loadDictionary(lingua);
+        
+        String input = txtInsert.getText();
+        String [] inputAr= input.split(" ");
+        
+        for(String s:inputAr) 
+        	if(s!=null)
+              inputTextList.add(s);
+        
+        List <RichWord> tempRW= new LinkedList<RichWord>(this.model.spellCheckText(inputTextList));
+        
+        for(RichWord r:tempRW)
+        	if(r!=null)
+        		resultTXT+=r.getWord()+"\n";
+        
+        txtError.setText("Ci sono "+tempRW.size()+" errori");
+        
+        txtResult.setText(resultTXT);
+        Long end = System.nanoTime();
+        txtTime.setText("Tempo impiegato :" + this.checkTime(start, end)+ "ms");
+    }
+    
+    public int checkTime(Long start, Long end) {
+    	return (int) (end-start);
+    }
+    @FXML
+    void initialize() {
+        assert choicheBox != null : "fx:id=\"choicheBox\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtInsert != null : "fx:id=\"txtInsert\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert btnSpell != null : "fx:id=\"btnSpell\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtResult != null : "fx:id=\"txtResult\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtError != null : "fx:id=\"txtError\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert btnClear != null : "fx:id=\"btnClear\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtTime != null : "fx:id=\"txtTime\" was not injected: check your FXML file 'Scene.fxml'.";
+        choicheBox.setItems(list);
+    }
+    void setModel(Model model) {
+    	this.model=model;
     }
 }
-
 
